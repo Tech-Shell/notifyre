1331c1f3afc9316e7c80de40920f14bcf1b4b64c
Anshul Saha <anshulsaha15@gmail.com>
2020-06-21 00:40:58 +0530

    added tasks.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('login', views.login, name="login"),
     path('dashboard', views.dashboard, name="dashboard"),
     path('logout', views.logout, name="logout"),
-    path('resetemail', views.reset_email, name="reset_email"),
-    path('resets', views.resets, name="resets"),
 ]
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
-
+    path('addtask', views.add_task, name="add_task"),
+    path('deletetask', views.delete_task, name="delete_task"),
+    path('updatetask', views.update_task, name="update_task"),
+    path('updatestask', views.updates_task, name="updates_task"),
 ]
