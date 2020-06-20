0d35f1a754a2f273f5a493a55384951f6eb6f0ee
Anshul Saha <anshulsaha15@gmail.com>
2020-06-20 19:18:26 +0530

    fixed urls.py issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register', views.register, name="register"),
     path('login', views.login, name="login"),
+    path('dashboard', views.dashboard, name="dashboard"),
 ]
