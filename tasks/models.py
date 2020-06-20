2f76f43a81e42325423edeb4f8af1975cbb2d413
Anshul Saha <anshulsaha15@gmail.com>
2020-06-21 00:25:30 +0530

    made migrations for task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    creation_date = models.DateTimeField(default = datetime.now)
+    user_id = models.IntegerField(blank = True)
+    task_status = models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
