ea6a2d9fbc31aef2109dfcfc948bb18d2e4ffaba
Anshul Saha <anshulsaha15@gmail.com>
2020-06-21 00:27:26 +0530

    modified accounts.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('login', views.login, name="login"),
     path('dashboard', views.dashboard, name="dashboard"),
+    path('logout', views.logout, name="logout"),
+    path('resetemail', views.reset_email, name="reset_email"),
+    path('resets', views.resets, name="resets"),
 ]
