30c16aea7af559c34b209ee0c43940f70ba7b9bd
Anshul Saha <anshulsaha15@gmail.com>
2020-06-20 17:24:15 +0530

    integrated the backend code for homepage

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'pages/home.html')
+
+
+
