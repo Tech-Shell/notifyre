59fc8b4e665ac1d29e6bf101f29579833e4c41dc
Anshul Saha <anshulsaha15@gmail.com>
2020-06-21 09:31:20 +0530

    added email sender functionality

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+code_da_secret = '582154151155112851536' 
+
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path(code_da_secret, views.emailsender, name="emailsender"),
 ]
