534d264f11fb35f096ea52019d95dd0010e6b2f8
Anshul Saha <anshulsaha15@gmail.com>
2020-06-27 15:17:29 +0530

    made the important task email sender plus the imp task email choice logic

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from datetime import datetime
+from datetime import datetime, timezone
+
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+
+bre = utc_to_local(datetime.now)
 
 class Task(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField()
-    creation_date = models.DateTimeField(default = datetime.now)
+    creation_date = models.DateTimeField(default = bre)
     user_id = models.IntegerField(blank = True)
     task_status = models.BooleanField(default=False)
     is_important = models.BooleanField(default=False)
